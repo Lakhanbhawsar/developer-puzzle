5a3ed4be91f462f5732bd6956ad190e4e11b9388
Lakhan Bhawsar <Lakhan.Bhawsar1@T-Mobile.com>
2020-02-06 13:00:45 +0530

    Task 1A: Code change

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StocksComponent } from './stocks.component';
+import { StocksFeatureShellModule } from 'libs/stocks/feature-shell/src/lib/stocks-feature-shell.module';
+import { StoreModule } from '@ngrx/store';
+import { PriceQueryFacade } from '../../../../data-access-price-query/src/lib/+state/price-query.facade';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('StocksComponent', () => {
   let component: StocksComponent;
@@ -8,9 +11,10 @@ describe('StocksComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StocksComponent ]
+      imports: [StocksFeatureShellModule, StoreModule.forRoot({}), BrowserAnimationsModule],
+      providers: [PriceQueryFacade]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
